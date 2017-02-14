0ffcddf7c1c09124cd4d089ed2fc07403beecc7f
iqbal <iqbalchoudhury2@gmail.com>
2017-02-14 18:52:21 -0500

    Step 8 finish working on middle java file

--- src/main/java/com/stta/utility/Read_XLS.java
+++ src/main/java/com/stta/utility/Read_XLS.java
@@ -7,6 +7,7 @@ import org.apache.poi.hssf.usermodel.HSSFCell;
 import org.apache.poi.hssf.usermodel.HSSFRow;
 import org.apache.poi.hssf.usermodel.HSSFSheet;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
+import org.apache.poi.ss.usermodel.Cell;
 
 public class Read_XLS {
 	
@@ -104,10 +105,205 @@ public class Read_XLS {
 		
 		//return null;
 	}
-
+	
+	//to retieve Data ToRun flasg of test data
+	public String [] retriveToRunFlagTestData(String wsName,String colName){
+		
+		int SheetIndex =wb.getSheetIndex(wsName);
+		if(SheetIndex==-1){
+			return null;
+		}
+		else{
+			int rowNum=retriveNoOfRows(wsName);
+			int colNum=retrieveNoOfCols(wsName);
+			int colNumber=-1;
+			
+			HSSFRow SuiteRow=ws.getRow(0);
+			String data[] =new String [rowNum-1];
+			
+			for(int i=0;i<colNum;i++){
+				if(SuiteRow.getCell(i).getStringCellValue().equals(colName.trim())){
+					colNumber=i;
+				}
+			}
+			
+			if(colNumber==-1){
+				return null;
+			}
+			for(int j=0;j<rowNum-1;j++){
+				HSSFRow Row=ws.getRow(j+1);
+				if(Row ==null){
+					data[j]="";
+					
+				}
+				else{
+					HSSFCell cell=Row.getCell(colNumber);
+					if(cell==null){
+						data[j]="";
+						
+					}
+					else{
+						String value=cellToString(cell);
+						data[j]=value;
+					}
+				}
+			}
+			return data;
+		}
+	}
+	
+	//To Retrive test data from test case data sheelt
+	public Object[][] retriveTestData(String wsName){
+		int sheetIndex=wb.getSheetIndex(wsName);
+		if(sheetIndex==-1){
+			return null;
+		}
+		
+		else{
+			int rowNum=retriveNoOfRows(wsName);
+			int colNum=retrieveNoOfCols(wsName);
+			Object data[] []=new Object[rowNum-1][colNum-2];
+			
+			for (int i=0;i<rowNum-1;i++){
+				HSSFRow row=ws.getRow(i+1);
+				for(int j=0; j<colNum-2;j++){
+					if(row==null){
+						data[i][j]="";
+					}
+					else{
+						HSSFCell cell=row.getCell(j);
+						if(cell==null){
+							data[i][j]="";
+							
+						}
+						else{
+							cell.setCellType(Cell.CELL_TYPE_STRING);
+							String value=cellToString(cell);
+							data[i][j]=value;
+						}
+					}
+				}
+			}
+			return data;
+		}
+		
+	}
+	
+	
 	private String cellToString(HSSFCell cell) {
 		// TODO Auto-generated method stub
-		return null;
+		
+		int type;
+		Object result;
+		switch(type){
+		case 0: 
+			result=cell.getNumericCellValue();
+			break;
+		case 1:
+			result=cell.getStringCellValue();
+			break;
+		default:
+			throw new RuntimeException("Unsupported cell.");
+		}
+		return result.toString();
 	}
+	
+	//tot write result in test data and test case list sheet
+	
+	public boolean writeResult(String wsName,String colName,int rowNumber,String Result){
+		
+		
+		try{
+			int sheetIndex=wb.getSheetIndex(wsName);
+			if(sheetIndex==-1){
+				return false;
+			}
+			int colNum=retrieveNoOfCols(wsName);
+			int colNumber=-1;
+			HSSFRow SuiteRow=ws.getRow(0);
+			for(int i=0;i<colNum;i++){
+				if(SuiteRow.getCell(i).getStringCellValue().equals(colName.trim())){
+					colNumber=i;
+					
+				}
+				
+			}
+			if (colNumber==-1){
+				return false;
+			}
+			HSSFRow Row=ws.getRow(rowNumber);
+			HSSFCell cell=Row.getCell(colNumber);
+			if(cell==null){
+				cell=Row.createCell(colNumber);
+				cell.setCellValue(Result);
+				opstr=new FileOutputStream(filelocation);
+				wb.write(opstr);
+				opstr.close();
+			}
+		}
+		
+		catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+		return true;
+	
 
+	}
+	
+	//To write result in test suite list sheet 
+	public boolean writeResult (String wsName, String colName, String rowName,String Result)
+	{
+		try
+		{
+			int rowNum=retriveNoOfRows(wsName);
+			int rowNumber=-1;
+			int sheetIndex=wb.getSheetIndex(wsName);
+			if(sheetIndex==-1){
+				return false;
+			}
+			int colNum=retrieveNoOfCols(wsName);
+			int colNumber=-1;
+			HSSFRow SuiteRow=ws.getRow(0);
+			for (int i=0;i<colNum;i++){
+				if(SuiteRow.getCell(i).getStringCellValue().equals(colName.trim())){
+					colNumber=i;
+					
+				}
+				
+				
+			}
+			if(colNumber==-1){
+				return false;
+			}
+			
+			
+			for (int i=0;i<rowNum-1;i++){
+				HSSFRow row=ws.getRow(i+1);
+				HSSFCell cell=row.getCell(0);
+				cell.setCellType(Cell.CELL_TYPE_STRING);
+				String value=cellToString(cell);
+				if(value.equals(rowName)){
+					rowNumber=i+1;
+					break;
+					
+				}
+			}
+
+		HSSFRow Row=ws.getRow(rowNumber);
+		HSSFCell cell=Row.getCell(colNumber);
+		if(cell==null)
+			cell=Row.createCell(colNumber);
+			cell.setCellValue(Result);
+			opstr.close();
+			
+	}
+		catch(Exception e){
+			e.printStackTrace();
+			return false;
+		}
+	return true;
+	
+	
+	
 }
